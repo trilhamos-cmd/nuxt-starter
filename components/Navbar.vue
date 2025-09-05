4ebe6171778d34e2b7d982b8cf4d87bf2c8edfb8
trilhamos-cmd <trilhamos@gmail.com>
2025-09-05 11:20:23 -0300

    Update Navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -15,8 +15,8 @@ const menuitems = [
   <Container>
     <header class="flex flex-row flex-wrap items-center justify-between my-4 gap-x-6 gap-y-4">
       <div class="flex items-center justify-between">
-        <a href="/" class="text-2xl font-bold text-zinc-800">
-          <span class="text-green-500">&#10070;</span> Nuxt Starter
+        <a href="/" class="flex items-center">
+  <NuxtImg src="/images/logo.png" alt="Meu Logo" class="h-10" />
         </a>
       </div>
       <nav class="flex w-auto">
